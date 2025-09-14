4e233773685876e7f6ee4ec02cbdab2675b8b07a
CamiloPosada <camiloposada1005@gmail.com>
2025-09-13 20:34:52 -0500

    update/changes

--- first_proyect/my_first_app/models.py
+++ first_proyect/my_first_app/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
-    title = models.TextField(Max_lengt=250)
+    title = models.TextField(max_length=250)
--- first_proyect/my_first_app/views.py
+++ first_proyect/my_first_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-def My_view(request):
-    render(request, "car_list.html", )
-    
+def my_view(request):
+    car_list = [
+        {"title": "BMW"},
+        {"title" : "Mazda"}
+    ]
+    context = {
+        "car_list": car_list
+    }
+    return render(request, "my_first_app/car_list.html", context)
